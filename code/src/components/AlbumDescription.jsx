a2555abe2fc972e971bf4872d1ec0a5d05614f65
Calle Gustafsson <calle.gustafsson@klarna.com>
2021-04-03 13:36:37 +0200

    Challenge yourself part 1

--- code/src/components/AlbumDescription.jsx
+++ code/src/components/AlbumDescription.jsx
@@ -7,15 +7,16 @@ export const AlbumDescription = (props) => {
     albumTitle
   } = props
   const renderArtists = artists.map(({ name, externalUrls }, index) => {
+    const delimiter = index + 2 === artists.length ? ' & ' : ' , '
     return (
-      <><a key={index} href={externalUrls.spotify} target="_blank" rel="noreferrer">{name}</a>{props.artists.length - 1 > index && ' , '}</>
+      <><a key={index} href={externalUrls.spotify} target="_blank" rel="noreferrer">{name}</a>{props.artists.length - 1 > index && delimiter}</>
     )
   })
 
   return (
     <div className="description">
       <span className="title"><a href={albumUrl} target="_blank" rel="noreferrer">{albumTitle}</a></span>
-      <span className="artists">{renderArtists}</span>
+      <span className="artists">{renderArtists}{renderArtists.length > 1 && ` (${renderArtists.length})`}</span>
     </div>
   )
 }
